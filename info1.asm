0f4f035d8f5e346510bf08f3bc12e75a84ae4363
517030910405 <42196261+517030910405@users.noreply.github.com>
2019-05-12 01:49:11 +0800

    Add files via upload

---
+++ info1.asm
@@ -0,0 +1,39 @@
+
+
+
+
+global __buffer_cache__
+global set_func
+global seek_func
+global init_func
+global toString
+global print
+global println
+global fifib
+global mallocx8
+global parseInt
+global str_add
+global str_le
+global str_ge
+global str_l
+global str_g
+global str_e
+global str_ne
+global getString
+global str_len
+global printlnInt
+global getInt
+global substring
+global ord
+;global main
+;global m
+global func_num
+global func_x
+global func_valid
+global func_y
+global n
+global func_valid_arr
+global func_x_arr
+global func_y_arr
+
+
